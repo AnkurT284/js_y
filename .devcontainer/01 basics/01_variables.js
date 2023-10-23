80e06b16178ae137c0aceffdcae2ca0858c8881f
AnkurT284 <122141255+AnkurT284@users.noreply.github.com>
2023-10-23 10:38:45 +0000

    variables

---
+++ .devcontainer/01 basics/01_variables.js
@@ -0,0 +1,8 @@
+const accountID = 13345
+let accountEmail = "ankurt@gmail.com"
+var accountPass ="12345"
+ // choose let over var//
+
+ console.log(accountID);
+ console.table(); // prints multiple values in tabular form\\
+ 
